be8033280c9c4f7fc8d56ca49223fd88917f781a
tobiasjiremyrhertz <tobias.jiremyrhertz@gmail.com>
2021-11-20 22:18:46 +0100

    job done

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 //Array med skämt
-const jokes = ["Hur många bor det i Stockholm? – En dryg miljon!","Hur tröstar man en vegan?– Soja, soja!","Hur vet man att en dykarkurs är inställd? Instruktören dyker inte upp!","Vad säger en Göteborgare till ett Star Wars fan? – JeeDaaaj.", "Vad sa den ena kannibalen till den andra? – E du go eller?","Varför är det ingen snö på gatorna i Glasgow på vintern? – De e la klart. 600 000 skottar.","Gick det bra med rymdresan? – Nej, den blev uppskjuten."]
-
+const jokes = ["Hur många bor det i Stockholm? –","Hur tröstar man en vegan? –","Hur vet man att en dykarkurs är inställd? - ","Vad säger en Göteborgare till ett Star Wars fan? –", "Vad sa den ena kannibalen till den andra? –","Varför är det ingen snö på gatorna i Glasgow på vintern? –","Gick det bra med rymdresan? –"]
+const punchLine = [" En dryg miljon!", " Soja, soja!", " Instruktören dyker inte upp!"," JeeDaaaj."," E du go eller?"," De e la klart. 600 000 skottar.", " Nej, den blev uppskjuten."]
 
 //funktion som spottar ur sig random skämt
 var randomJoke = () => {
-    var randInt = Math.floor(Math.random()*(jokes.length));
-    console.log(jokes[randInt]);
+    var randomJoke = Math.floor(Math.random()*(jokes.length));
+    var randomPunchLine = Math.floor(Math.random()*(punchLine.length));
+    console.log(jokes[randomJoke]+punchLine[randomPunchLine]);
 }
 
 randomJoke();
