16a43ac916e564bead84e021ddc33907a28c319e
tobiasjiremyrhertz <tobias.jiremyrhertz@gmail.com>
2021-11-20 21:49:29 +0100

    add more jokes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 //Array med skämt
-const jokes = ["Vad säger en Göteborgare till ett Star Wars fan? – JeeDaaaj.", "Vad sa den ena kannibalen till den andra? – E du go eller?","Varför är det ingen snö på gatorna i Glasgow på vintern? – De e la klart. 600 000 skottar.","Gick det bra med rymdresan? – Nej, den blev uppskjuten."]
+const jokes = ["Hur många bor det i Stockholm? – En dryg miljon!","Hur tröstar man en vegan?– Soja, soja!","Hur vet man att en dykarkurs är inställd? Instruktören dyker inte upp!","Vad säger en Göteborgare till ett Star Wars fan? – JeeDaaaj.", "Vad sa den ena kannibalen till den andra? – E du go eller?","Varför är det ingen snö på gatorna i Glasgow på vintern? – De e la klart. 600 000 skottar.","Gick det bra med rymdresan? – Nej, den blev uppskjuten."]
 
 
 //funktion som spottar ur sig random skämt
