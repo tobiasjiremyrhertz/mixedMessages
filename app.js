decaad4ea5efe7ac65cbcf38ef8e40225a83e036
tobiasjiremyrhertz <tobias.jiremyrhertz@gmail.com>
2021-11-20 21:37:30 +0100

    create app.js and main function

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Array med skämt
+const jokes = ["Vad säger en Göteborgare till ett Star Wars fan? – JeeDaaaj.", "Vad sa den ena kannibalen till den andra? – E du go eller?","Varför är det ingen snö på gatorna i Glasgow på vintern? – De e la klart. 600 000 skottar.","Gick det bra med rymdresan? – Nej, den blev uppskjuten."]
+
+
+//funktion som spottar ur sig random skämt
+var randomJoke = () => {
+    var randInt = Math.floor(Math.random()*(jokes.length+1));
+    console.log(randInt);
+}
+
+randomJoke();
--- readMe.md
+++ readMe.md
@@ -1,1 +1,2 @@
 # This program is a message generator that randomizes output to different messages
+## It tells you Göteborsskämt on demand!
